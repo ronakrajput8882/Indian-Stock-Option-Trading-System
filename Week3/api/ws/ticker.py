e5fb1f5ed523f3c3e3bf7c4a19e80867e27df6b5
ronakrajput8882 <ronakrajput160805@gmail.com>
2026-04-21 20:53:04 +0530

    week 3 ticket.py change

--- Week3/api/ws/ticker.py
+++ Week3/api/ws/ticker.py
@@ -1,13 +1,3 @@
 import asyncio
 import websockets
 import json
-
-class TickerWebSocket:
-    def __init__(self, uri):
-        self.uri = uri
-    
-    async def connect(self):
-        async with websockets.connect(self.uri) as websocket:
-            while True:
-                data = await websocket.recv()
-                print(json.loads(data))
